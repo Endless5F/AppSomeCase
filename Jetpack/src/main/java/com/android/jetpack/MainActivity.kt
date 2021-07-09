f90b10abe625ed8cb8a8e9cccc16506bbafe5b42
jiaochengyun <jiaochengyun@baidu.com>
2021-07-09 14:13:12 +0800

    Hello World

---
+++ Jetpack/src/main/java/com/android/jetpack/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.android.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("World")
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text (text = "Hello $name!")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("Android")
+    }
+}
