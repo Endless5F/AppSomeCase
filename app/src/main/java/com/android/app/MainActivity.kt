e1e0124112ec0fde76ad92b44bd04a165e1f7fe1
jiaochengyun <jiaochengyun@baidu.com>
2021-11-18 17:16:37 +0800

    ApiFactory test

--- app/src/main/java/com/android/app/MainActivity.kt
+++ app/src/main/java/com/android/app/MainActivity.kt
@@ -2,6 +2,11 @@ package com.android.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.core.restful.HiCallback
+import com.android.core.restful.HiResponse
+import com.android.core.restful.demo.ApiFactory
+import com.android.core.restful.demo.TestApi
 import com.android.kmmshared.Greeting
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApiFactory.create(TestApi::class.java).getArticleList(0)
+            .enqueue(object : HiCallback<String> {
+                override fun onSuccess(response: HiResponse<String>) {
+                    Log.e("jcy", "onSuccess: ${response.data}")
+                }
+
+                override fun onFailed(throwable: Throwable) {
+                    Log.e("jcy", "onFailed: ${throwable.message}")
+                }
+            })
 
         Greeting().greeting()
     }
